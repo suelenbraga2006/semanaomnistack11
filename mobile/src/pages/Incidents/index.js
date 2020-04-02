3bb105d2597e4395b4d4b8e578dc9066c7837a58
Suelen Braga <suelen.braga2006@gmail.com>
2020-04-02 18:04:35 -0300

    Mobile correções

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -30,7 +30,7 @@ export default function Incidents() {
         setLoading(true);
 
         const response = await api.get('incidents', { params: {pages} });
-        setIncidents([... incidents. ... response.data]);
+        setIncidents([... incidents, ... response.data]);
         setTotal(response.headers['x-total-count']);
         setPage(page + 1);
 
